34347a1b8bcae03f5cda32549d45cc6370bcbe89
Ghanshyam Anand <ghanshyam@codingninjas.in>
2018-10-30 12:27:50 +0530

    shepherd implementation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare let Shepherd: any;
+
 
 @Component({
   selector: 'app-header',
@@ -10,6 +12,77 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const tour = new Shepherd.Tour({
+      defaults: {
+        classes: 'shepherd-theme-arrows',
+        scrollTo: true
+      }
+    });
+
+    tour.addStep('example-step', {
+      text: 'This is home, click here to go to home page.',
+      attachTo: '#id1 bottom',
+      classes: 'example-step-extra-class',
+      buttons: [
+        {
+          text: 'Next',
+          action: tour.next
+        },
+        {
+          text: 'Exit',
+          classes: 'shepherd-button-secondary',
+          action: tour.complete
+        }
+      ]
+    });
+
+    tour.addStep('example-step', {
+      text: 'This is news link, click here to go to news page.',
+      attachTo: '#id2 bottom',
+      classes: 'example-step-extra-class',
+      buttons: [
+        {
+          text: 'Back',
+          action: tour.back,
+          classes: 'shepherd-button-example-primary'
+        },
+        {
+          text: 'Next',
+          action: tour.next
+        },
+        {
+          text: 'Exit',
+          classes: 'shepherd-button-secondary',
+          action: tour.complete
+        }
+      ]
+    });
+
+    tour.addStep('example-step', {
+      text: 'This is footer.',
+      attachTo: '#footer_id bottom',
+      classes: 'example-step-extra-class',
+      buttons: [
+        {
+          text: 'Back',
+          action: tour.back,
+          classes: 'shepherd-button-example-primary'
+        },
+        {
+          text: 'Next',
+          action: tour.next
+        },
+        {
+          text: 'Exit',
+          classes: 'shepherd-button-secondary',
+          action: tour.complete
+        }
+      ]
+    });
+
+    tour.start();
+
   }
 
 }
