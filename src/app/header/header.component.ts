070c90f3fd72e739cd5643ac669c42009b4c61af
Ghanshyam Anand <ghanshyam@codingninjas.in>
2018-10-30 12:52:44 +0530

    Into js Implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let introJs: any;
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,36 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const intro = introJs();
+// Initialize steps
+    intro.setOptions({
+      steps: [
+        {
+          element: '#home_link',
+          intro: 'Step one description',
+          position: 'bottom'
+        },
+        {
+          element: '#news_link',
+          intro: 'Step <i>two</i> description',
+          position: 'bottom'
+        },
+        {
+          element: '#contact_link',
+          intro: 'Step <span style="color: green;">three</span> description',
+          position: 'bottom'
+        },
+        {
+          element: '#footer',
+          intro: 'Step <span style="color: green;">three</span> description',
+          position: 'up'
+        }
+
+      ]
+    });
+
+    intro.start();
+
   }
 
 }
